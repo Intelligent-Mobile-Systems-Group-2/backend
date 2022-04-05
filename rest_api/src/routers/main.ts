7adb07e0d8525c6d5e573e706c53d94efd6999c6
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2022-04-05 16:40:52 +0200

    image post request with a timeout function

--- rest_api/src/routers/main.ts
+++ rest_api/src/routers/main.ts
@@ -14,4 +14,33 @@ router.post('/user', async (ctx, next) => {
   }`;
 });
 
+router.post('/image', async(ctx, next) => {
+  const imageInput:string = ctx.request.body.imageBase64;
+  if (!imageInput) {
+    ctx.throw(400, 'image field required');
+  }
+
+  // Send image to Google Vision API
+  ctx.body = await requestToGoogleAPI(imageInput);
+
+});
+
+async function requestToGoogleAPI(imageBase64:string){
+    await resolveAfter2Seconds();
+    return {
+      label: "It is Dwayne (The Rock) Johnson!",
+      imageBase64: imageBase64
+    }
+}
+
+// Timeout function
+function resolveAfter2Seconds() {
+  var x = 10;
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
+
 export default router;
